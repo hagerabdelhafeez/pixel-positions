700d3831bdce0feb49a9705247f7cd8f4f1b2484
Hager Abdo <hager.abdo121@hotmail.com>
2025-04-27 23:22:29 +0300

    edit views

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-1000">Video Producer</h3>
+        <p class="text-xs text-gray-400 mt-4">Full Time - From $60,000</p>
+    </div>
+    <div>
+        <x-tag>Tage</x-tag>
+        <x-tag>Tage</x-tag>
+        <x-tag>Tage</x-tag>
+    </div>
+</x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,16 @@
-<div class="p-4 bg-white/5 rounded-lg flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-1000">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tage</x-tag>
-            <x-tag>Tage</x-tag>
-            <x-tag>Tage</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
-        <img src="https://placehold.co/42x42/blue/white" alt="" class="rounded-xl">
+        <x-employer-logo :width="42" />
     </div>
-</div>
+</x-panel>
 
